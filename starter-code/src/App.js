a3af6ae23db5364b6ccf77c8a5ae3a478f448279
Fabio Menozzi <f.menozzi@yahoo.com>
2020-04-29 21:38:58 -0300

    app.js adjustments

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -3,16 +3,74 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      contactsArr: contacts.splice(0,5),
+    }
+
+    this.addRandom = this.addRandom.bind(this)
+    this.sortByName = this.sortByName.bind(this)
+    this.sortByPopularity = this.sortByPopularity.bind (this)
+  }
+
+  addRandom() {
+    
+    const randNum = Math.floor(Math.random() * contacts.length);
+    const contactsArrCopy = [...this.state.contactsArr]
+
+    contactsArrCopy.push(contacts[randnum])
+    contacts.splice(randNum, 1)
+
+
+    this.setState({
+      contactsArr: contactsArrCopy,
+    })
+  }
+
+
+  sortByName() {
+    const contactsArrCopy = [...this.state.contactsArr]
+    constactsArrCopy.sort((a, b) => a.name.localeCompare(b.name))
+    
+    this.setState({
+      contactsArr: contactsArrCopy,
+    })
+  }
+
+  sortByPopularity() {
+    const contactsArrCopy = [...this.state.contactsArr]
+    constactsArrCopy.sort((a, b) => b.popularity - a.popularity)
+    
+    this.setState({
+      contactsArr:  contactsArrCopy,
+    })
+  }
+
+
+  deleteContact(idx) {
+    const contactsArrCopy = [...this.state.contactsArr]
+
+    contactsArrCopy.splice(idx, 1)
+    
+    this.setState({
+      contactsArr: constactsArrCopy,
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header
+        
+          addRandom={this.addRandom}
+          sortByName={this.sortByName}
+          sortByPopularity={this.sortByPopularity}
+        />
+        <Table contactsArr={this.state.contactsArr}/>
       </div>
     );
   }
